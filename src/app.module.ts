d16a8ee424ecbbef68177f1724692f26aa13612d
Maksym <qlowlight@gmail.com>
2025-10-25 16:57:11 +0300

    connect auth and users modules, connect typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './modules/iam/auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5430,
+      username: 'admin',
+      password: 'qwerty',
+      database: 'blog',
+      synchronize: false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

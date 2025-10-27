e4bc66f6b5b1069af862aa9db6d5da1f238473d5
Maksym <qlowlight@gmail.com>
2025-10-27 19:42:42 +0200

    connect exception filter global, config exception factory in validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { HttpException, ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './common/exception/exception-filter/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      stopAtFirstError: true,
+      exceptionFactory: (errors) => {
+        const errorsViewModel: { message: string; field: string }[] = [];
+
+        errors.forEach((e) => {
+          if (!e.constraints) return;
+
+          const errViewModel = {
+            message: Object.values(e.constraints)[0],
+            field: e.property,
+          };
+
+          errorsViewModel.push(errViewModel);
+        });
+
+        throw new HttpException(errorsViewModel, 400);
+      },
+    }),
+  );
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();

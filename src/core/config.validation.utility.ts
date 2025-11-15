3d3b703905817d97e325922ed99ed0945e569eca
Maksym <qlowlight@gmail.com>
2025-11-16 00:36:03 +0200

    add convert into boolean static method for config.validation.utility.ts

--- src/core/config.validation.utility.ts
+++ src/core/config.validation.utility.ts
@@ -5,12 +5,24 @@ export class ConfigValidationUtility {
     const errors = validateSync(config);
     if (errors.length > 0) {
       const sortedMessages = errors.map((e: ValidationError) => {
-        const currentValue: any = e.value || '';
+        console.log(e);
+        const currentValue = (e.value || '') as string;
         const constraint = Object.values(e.constraints || {}).join(', ');
         return `${constraint} (currentValue: ${currentValue})`;
       });
 
       throw new Error(sortedMessages.join('; \n'));
     }
+  }
+
+  static convertToBoolean(value: string | undefined) {
+    if (!value) return null;
+    const trimmedValue = value.trim();
+    const trueValues = ['true', '1', 'enabled'];
+    const falseValues = ['false', '0', 'disabled'];
+
+    if (trueValues.includes(trimmedValue)) return true;
+    if (falseValues.includes(trimmedValue)) return false;
+    return value;
   }
 }

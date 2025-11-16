88c6e1fa8e45a57a7a2d6302218bfc06b1eefc38
Maksym <qlowlight@gmail.com>
2025-11-16 22:50:57 +0200

    add throwExceptionFromCustomErr, refactoring

--- src/modules/iam/auth/controllers/auth.controller.ts
+++ src/modules/iam/auth/controllers/auth.controller.ts
@@ -1,14 +1,9 @@
-import {
-  BadRequestException,
-  Body,
-  Controller,
-  Get,
-  Post,
-} from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AuthService } from '../application/auth.service';
 import { CreateUserDto } from '../../users/dto/create-user.dto';
 import { ResultInputError } from '../../../../common/helpers/result/result-error';
 import { ResultType } from '../../../../common/helpers/result/result';
+import { throwExceptionFromCustomErr } from '../../../../common/exception/throw-exception-from-custom-err';
 
 @Controller('auth')
 export class AuthController {
@@ -19,10 +14,11 @@ export class AuthController {
 
   @Post('registration')
   async registration(@Body() dto: CreateUserDto) {
-    const result: ResultType<null, ResultInputError> =
+    const result: ResultType<string, ResultInputError> =
       await this.authService.registration(dto);
-    if (!result.isSuccessful)
-      throw new BadRequestException([result.error.message]);
-    return result.content;
+
+    if (result.isSuccessful) return result.content;
+
+    throwExceptionFromCustomErr(result.error);
   }
 }

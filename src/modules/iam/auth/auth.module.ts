ec1d403f8382de547a4fe682af897440c2ed8e53
Maksym <qlowlight@gmail.com>
2025-11-16 15:25:38 +0200

    import EmailModule into AuthModule

--- src/modules/iam/auth/auth.module.ts
+++ src/modules/iam/auth/auth.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './controllers/auth.controller';
 import { AuthService } from './application/auth.service';
 import { UsersModule } from '../users/users.module';
+import { EmailModule } from '../../../common/services/email/email.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, EmailModule],
   controllers: [AuthController],
   providers: [AuthService],
 })

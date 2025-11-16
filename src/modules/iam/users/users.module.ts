dbe35cefc4761ee80c1e10e1d65715c6b4498075
Maksym <qlowlight@gmail.com>
2025-11-16 19:43:27 +0200

    add new repositories into user module

--- src/modules/iam/users/users.module.ts
+++ src/modules/iam/users/users.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './application/users.service';
 import { UsersRepository } from './repositories/users.repository';
 import { PasswordHashService } from '../../../common/security/password-hash.service';
+import { UsersConfirmationRepository } from './repositories/users-confirmation.repository';
+import { UsersPasswordRecoveryRepository } from './repositories/users-password-recovery.repository';
 
 @Module({
   controllers: [],
-  providers: [UsersService, UsersRepository, PasswordHashService],
+  providers: [
+    UsersService,
+    PasswordHashService,
+    UsersRepository,
+    UsersConfirmationRepository,
+    UsersPasswordRecoveryRepository,
+  ],
   exports: [UsersService, UsersRepository],
 })
 export class UsersModule {}

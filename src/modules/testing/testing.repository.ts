cca1085fbb22efb5556e02648ad2eb14226b4857
Maksym <qlowlight@gmail.com>
2025-11-18 19:47:05 +0200

    edit test endpoint, now it delete all data from all tables

--- src/modules/testing/testing.repository.ts
+++ src/modules/testing/testing.repository.ts
@@ -7,6 +7,14 @@ export class TestingRepository {
   constructor(@InjectDataSource() protected dataSource: DataSource) {}
 
   async deleteAllDbData() {
-    await this.dataSource.query(`DELETE FROM users`);
+    await this.dataSource.query(`DO $$ 
+DECLARE 
+    r RECORD;
+BEGIN 
+    FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = current_schema()) 
+    LOOP 
+        EXECUTE 'TRUNCATE TABLE ' || quote_ident(r.tablename) || ' CASCADE'; 
+    END LOOP; 
+END $$;`);
   }
 }

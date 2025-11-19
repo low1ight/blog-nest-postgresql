873c52ee9b9b94efe36c19d0625d0b6e264a4a40
Maksym <qlowlight@gmail.com>
2025-11-19 21:25:06 +0200

    add sending confirmation code by nodemailer if user successful created

--- src/modules/iam/auth/application/auth.service.ts
+++ src/modules/iam/auth/application/auth.service.ts
@@ -1,12 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from '../../users/application/users.service';
 import { CreateUserDto } from '../../users/dto/create-user.dto';
+import { Result, ResultType } from '../../../../common/helpers/result/result';
+import { ResultInputError } from '../../../../common/helpers/result/result-error';
+import { EmailManager } from '../../../../shared/email/email.manager';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly emailManager: EmailManager,
+  ) {}
 
   async registration(dto: CreateUserDto) {
-    return this.usersService.creatUser(dto);
+    const result: ResultType<string, ResultInputError> =
+      await this.usersService.creatUser(dto);
+
+    if (!result.isSuccessful) return result;
+
+    this.emailManager.sendRegistrationCode(dto.email, result.content);
+
+    return Result.ok();
   }
 }

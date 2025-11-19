ac89b01a221a02583bcd726d502acba727bb5280
Maksym <qlowlight@gmail.com>
2025-11-19 21:23:10 +0200

    add users config import

--- src/modules/iam/users/users.module.ts
+++ src/modules/iam/users/users.module.ts
@@ -4,11 +4,13 @@ import { UsersRepository } from './repositories/users.repository';
 import { UsersConfirmationRepository } from './repositories/users-confirmation.repository';
 import { UsersPasswordRecoveryRepository } from './repositories/users-password-recovery.repository';
 import { PasswordHashModule } from '../../../shared/passwordHash/password-hash.module';
+import { UsersConfig } from './config/users.config';
 
 @Module({
   controllers: [],
   imports: [PasswordHashModule],
   providers: [
+    UsersConfig,
     UsersService,
     UsersRepository,
     UsersConfirmationRepository,

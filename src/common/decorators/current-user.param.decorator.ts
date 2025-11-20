bfc92c894d1be5e2a5fd72014c8a401ef00aec96
Maksym <qlowlight@gmail.com>
2025-11-20 23:09:58 +0200

    add current user param decorator

---
+++ src/common/decorators/current-user.param.decorator.ts
@@ -0,0 +1,14 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { UserPayloadModel } from '../../modules/iam/auth/types/user-payload.model';
+import { Request } from 'express';
+
+export const CurrentUser = createParamDecorator(
+  (ctx: ExecutionContext): UserPayloadModel => {
+    const request: Request = ctx.switchToHttp().getRequest();
+    const user: UserPayloadModel = request?.user as UserPayloadModel;
+
+    if (!user) throw Error('User not found! Set local auth guard!');
+
+    return user;
+  },
+);

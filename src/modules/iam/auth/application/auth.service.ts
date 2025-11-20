00c503d32db9abf76a13d85b9eff991f6c992216
Maksym <qlowlight@gmail.com>
2025-11-20 23:10:40 +0200

    add validateUser method

--- src/modules/iam/auth/application/auth.service.ts
+++ src/modules/iam/auth/application/auth.service.ts
@@ -4,12 +4,17 @@ import { CreateUserDto } from '../../users/dto/create-user.dto';
 import { Result, ResultType } from '../../../../common/helpers/result/result';
 import { ResultInputError } from '../../../../common/helpers/result/result-error';
 import { EmailManager } from '../../../../shared/email/email.manager';
+import { UsersRepository } from '../../users/repositories/users.repository';
+import { PasswordHashService } from '../../../../shared/passwordHash/password-hash.service';
+import { UserDocumentModel } from '../../users/models/user-document.model';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly usersService: UsersService,
     private readonly emailManager: EmailManager,
+    private readonly userRepository: UsersRepository,
+    private readonly passwordHashService: PasswordHashService,
   ) {}
 
   async registration(dto: CreateUserDto) {
@@ -21,5 +26,21 @@ export class AuthService {
     this.emailManager.sendRegistrationCode(dto.email, result.content);
 
     return Result.ok();
+  }
+
+  async validateUser(loginOrEmail: string, password: string) {
+    const user: UserDocumentModel | null =
+      await this.userRepository.getUserByEmailOrLogin(loginOrEmail);
+
+    if (!user) return null;
+
+    const isPasswordMatch = await this.passwordHashService.compare(
+      password,
+      user.password,
+    );
+
+    if (!isPasswordMatch) return null;
+
+    return user;
   }
 }

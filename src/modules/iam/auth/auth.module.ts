513e60ed2b34240d5b82e81b344c01c55af2b614
Maksym <qlowlight@gmail.com>
2025-11-20 23:10:24 +0200

    add imports

--- src/modules/iam/auth/auth.module.ts
+++ src/modules/iam/auth/auth.module.ts
@@ -3,10 +3,12 @@ import { AuthController } from './controllers/auth.controller';
 import { AuthService } from './application/auth.service';
 import { UsersModule } from '../users/users.module';
 import { EmailModule } from '../../../shared/email/email.module';
+import { PasswordHashModule } from '../../../shared/passwordHash/password-hash.module';
+import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
-  imports: [UsersModule, EmailModule],
+  imports: [UsersModule, EmailModule, PasswordHashModule],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, LocalStrategy],
 })
 export class AuthModule {}

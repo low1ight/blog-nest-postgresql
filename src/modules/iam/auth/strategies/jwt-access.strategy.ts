47ad435bf10920e2f25a13f386fbad376708a508
Maksym <qlowlight@gmail.com>
2025-11-22 00:04:21 +0200

    add jwt-access strategy

---
+++ src/modules/iam/auth/strategies/jwt-access.strategy.ts
@@ -0,0 +1,23 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { TokenConfig } from '../../../../core/services/jwt/token.config';
+import { AccessTokenPayloadModel } from '../types/access-token-payload.model';
+
+@Injectable()
+export class JwtAccessStrategy extends PassportStrategy(
+  Strategy,
+  'jwt-access',
+) {
+  constructor(tokenConfig: TokenConfig) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: tokenConfig.jwtSecret,
+    });
+  }
+
+  validate(payload: AccessTokenPayloadModel): AccessTokenPayloadModel {
+    return payload;
+  }
+}

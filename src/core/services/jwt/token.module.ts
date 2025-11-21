e5e1c9c22372b564852e82140752eea2497555c2
Maksym <qlowlight@gmail.com>
2025-11-21 23:58:47 +0200

    add token config into token module

--- src/core/services/jwt/token.module.ts
+++ src/core/services/jwt/token.module.ts
@@ -1,14 +1,18 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { TokenService } from './token.service';
+import { TokenConfig } from './token.config';
 
 @Module({
   imports: [
-    JwtModule.register({
-      secret: '1111',
+    JwtModule.registerAsync({
+      useFactory: (tokenConfig: TokenConfig) => ({
+        secret: tokenConfig.jwtSecret,
+      }),
+      inject: [TokenConfig],
     }),
   ],
-  providers: [TokenService],
-  exports: [TokenService],
+  providers: [TokenConfig, TokenService],
+  exports: [TokenService, TokenConfig],
 })
 export class TokenModule {}

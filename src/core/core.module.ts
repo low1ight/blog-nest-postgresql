a6ec522602ac394613e2a1c41ab9f7641a8ea7ac
Maksym <qlowlight@gmail.com>
2025-11-22 00:00:09 +0200

    add TokenModule into core module

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -2,11 +2,12 @@ import { Global, Module } from '@nestjs/common';
 import { CoreConfig } from './config/core.config';
 import { EmailModule } from './services/email/email.module';
 import { PasswordHashModule } from './services/passwordHash/password-hash.module';
+import { TokenModule } from './services/jwt/token.module';
 
 @Global()
 @Module({
-  imports: [EmailModule, PasswordHashModule],
+  imports: [EmailModule, PasswordHashModule, TokenModule],
   providers: [CoreConfig],
-  exports: [CoreConfig, EmailModule, PasswordHashModule],
+  exports: [CoreConfig, EmailModule, PasswordHashModule, TokenModule],
 })
 export class CoreModule {}

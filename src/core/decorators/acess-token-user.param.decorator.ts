b5e8d6f66215b9628218fed96826cc8114cdefe7
Maksym <qlowlight@gmail.com>
2025-11-22 22:45:46 +0200

    add access token user decorator

---
+++ src/core/decorators/acess-token-user.param.decorator.ts
@@ -0,0 +1,16 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { LoginUserPayloadModel } from '../../modules/iam/auth/types/login-user-payload.model';
+import { Request } from 'express';
+import { AccessTokenPayloadModel } from '../../modules/iam/auth/types/access-token-payload.model';
+
+export const AtUser = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext): AccessTokenPayloadModel => {
+    const request: Request = ctx.switchToHttp().getRequest();
+    const user: LoginUserPayloadModel =
+      request?.user as AccessTokenPayloadModel;
+
+    if (!user) throw Error('User not found! Set access token guard!');
+
+    return user;
+  },
+);

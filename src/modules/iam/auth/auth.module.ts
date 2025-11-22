dce1b4008f7ba4ed64f52ac1cabdd301d1a47618
Maksym <qlowlight@gmail.com>
2025-11-22 22:47:45 +0200

    add jwt strategies

--- src/modules/iam/auth/auth.module.ts
+++ src/modules/iam/auth/auth.module.ts
@@ -3,10 +3,17 @@ import { AuthController } from './controllers/auth.controller';
 import { AuthService } from './application/auth.service';
 import { UsersModule } from '../users/users.module';
 import { LocalStrategy } from './strategies/local.strategy';
+import { JwtAccessStrategy } from './strategies/jwt-access.strategy';
+import { JwtRefreshStrategy } from './strategies/jwt-refresh.strategy';
 
 @Module({
   imports: [UsersModule],
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy],
+  providers: [
+    AuthService,
+    LocalStrategy,
+    JwtAccessStrategy,
+    JwtRefreshStrategy,
+  ],
 })
 export class AuthModule {}

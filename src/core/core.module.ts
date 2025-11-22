7e6cf1ea1b5862d190c4e0cb1fadfa59cb8cf18b
Maksym <qlowlight@gmail.com>
2025-11-22 22:47:28 +0200

    import guards into core module

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -3,11 +3,27 @@ import { CoreConfig } from './config/core.config';
 import { EmailModule } from './services/email/email.module';
 import { PasswordHashModule } from './services/passwordHash/password-hash.module';
 import { TokenModule } from './services/jwt/token.module';
+import { JwtAccessAuthGuard } from './guards/jwt-access-auth.guard';
+import { JwtRefreshAuthGuard } from './guards/jwt-refresh-auth.guard';
+import { LocalAuthGuard } from './guards/local-auth.guard';
 
 @Global()
 @Module({
   imports: [EmailModule, PasswordHashModule, TokenModule],
-  providers: [CoreConfig],
-  exports: [CoreConfig, EmailModule, PasswordHashModule, TokenModule],
+  providers: [
+    CoreConfig,
+    JwtAccessAuthGuard,
+    JwtRefreshAuthGuard,
+    LocalAuthGuard,
+  ],
+  exports: [
+    CoreConfig,
+    EmailModule,
+    PasswordHashModule,
+    TokenModule,
+    JwtAccessAuthGuard,
+    JwtRefreshAuthGuard,
+    LocalAuthGuard,
+  ],
 })
 export class CoreModule {}

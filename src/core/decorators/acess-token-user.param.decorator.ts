5408f633f3051f9b7d8a653c91b46618198e1c7e
Maksym <qlowlight@gmail.com>
2025-12-02 20:59:01 +0200

    fix jwt auth decorators

--- src/core/decorators/acess-token-user.param.decorator.ts
+++ src/core/decorators/acess-token-user.param.decorator.ts
@@ -1,13 +1,12 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { LoginUserPayloadModel } from '../dto/login-user-payload.model';
+import { UserLoginModel } from '../dto/user-login.model';
 import { Request } from 'express';
 import { AccessTokenPayloadModel } from '../dto/access-token-payload.model';
 
 export const AtUser = createParamDecorator(
   (data: unknown, ctx: ExecutionContext): AccessTokenPayloadModel => {
     const request: Request = ctx.switchToHttp().getRequest();
-    const user: LoginUserPayloadModel =
-      request?.user as AccessTokenPayloadModel;
+    const user: UserLoginModel = request?.user as AccessTokenPayloadModel;
 
     if (!user) throw Error('User not found! Set access token guard!');
 
--- src/core/decorators/refresh-token-user.param.decorator.ts
+++ src/core/decorators/refresh-token-user.param.decorator.ts
@@ -1,13 +1,11 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { LoginUserPayloadModel } from '../dto/login-user-payload.model';
 import { Request } from 'express';
 import { RefreshTokenPayloadModel } from '../dto/refresh-token-payload.model';
 
 export const RtUser = createParamDecorator(
   (data: unknown, ctx: ExecutionContext): RefreshTokenPayloadModel => {
     const request: Request = ctx.switchToHttp().getRequest();
-    const user: LoginUserPayloadModel =
-      request?.user as RefreshTokenPayloadModel;
+    const user = request?.user as RefreshTokenPayloadModel;
 
     if (!user) throw Error('User not found! Set refresh token guard!');
 

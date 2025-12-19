6aac1310eb09b051837c89404a35578e5314fd8d
Maksym <qlowlight@gmail.com>
2025-12-19 19:39:51 +0200

    united modules into one auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,10 @@ import { CoreModule } from './core/core.module';
 import { DynamicModule, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './modules/iam/auth/auth.module';
+import { AuthModule } from './modules/iam/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoreConfig } from './core/config/core.config';
 import { TestingModule } from './modules/testing/testing.module';
-import { UsersModule } from './modules/iam/users/users.module';
-import { DevicesModule } from './modules/iam/devices/devices.module';
 import { CqrsModule } from '@nestjs/cqrs';
 
 @Module({
@@ -21,8 +19,6 @@ export class AppModule {
     const modules: any[] = [
       CqrsModule.forRoot(),
       AuthModule,
-      UsersModule,
-      DevicesModule,
       TypeOrmModule.forRootAsync({
         imports: [CoreModule],
         useFactory: (coreConfig: CoreConfig) => ({
---
+++ src/modules/iam/auth.module.ts
@@ -0,0 +1,69 @@
+import { Module } from '@nestjs/common';
+import { AuthController } from './auth/api/auth.controller';
+import { AuthService } from './auth/application/auth.service';
+import { LocalStrategy } from './auth/strategies/local.strategy';
+import { JwtAccessStrategy } from './auth/strategies/jwt-access.strategy';
+import { JwtRefreshStrategy } from './auth/strategies/jwt-refresh.strategy';
+import { Registration } from './auth/application/use-cases/registration.use-case';
+import { Login } from './auth/application/use-cases/login.use-case';
+import { Logout } from './auth/application/use-cases/logout.use-case';
+import { RefreshToken } from './auth/application/use-cases/refresh-token.use-case';
+import { PasswordRecovery } from './auth/application/use-cases/password-recovery.use-case';
+import { SetNewPassword } from './auth/application/use-cases/set-new-password.use-case';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Device } from './devices/domain/device.entity';
+import { DevicesController } from './devices/api/devices.controller';
+import { User } from './users/domain/user.entity';
+import { UserPasswordRecovery } from './users/domain/user-password-recovery.entity';
+import { UserConfirmation } from './users/domain/user-confirmation.entity';
+import { UsersConfig } from './users/config/users.config';
+import { UsersService } from './users/application/users.service';
+import { UsersRepository } from './users/infrastructure/users.repository';
+import { UsersQueryRepository } from './users/infrastructure/users.query.repository';
+import { UsersConfirmationRepository } from './users/infrastructure/users-confirmation.repository';
+import { UsersPasswordRecoveryRepository } from './users/infrastructure/users-password-recovery.repository';
+import { DevicesService } from './devices/application/devices.service';
+import { DevicesRepository } from './devices/infrastructure/devices.repository';
+import { DevicesQueryRepository } from './devices/infrastructure/devices.query.repository';
+import { TerminateAllOtherDevices } from './devices/application/use-cases/terminate-all-other-devices.use-case';
+import { TerminateSpecifiedDevice } from './devices/application/use-cases/terminate-specified-device.use-case';
+
+const useCases = [
+  Registration,
+  Login,
+  Logout,
+  RefreshToken,
+  PasswordRecovery,
+  SetNewPassword,
+  TerminateAllOtherDevices,
+  TerminateSpecifiedDevice,
+];
+
+@Module({
+  controllers: [AuthController, DevicesController],
+  imports: [
+    TypeOrmModule.forFeature([
+      User,
+      UserPasswordRecovery,
+      UserConfirmation,
+      Device,
+    ]),
+  ],
+  providers: [
+    AuthService,
+    LocalStrategy,
+    JwtAccessStrategy,
+    JwtRefreshStrategy,
+    UsersConfig,
+    UsersService,
+    UsersRepository,
+    UsersQueryRepository,
+    UsersConfirmationRepository,
+    UsersPasswordRecoveryRepository,
+    DevicesService,
+    DevicesRepository,
+    DevicesQueryRepository,
+    ...useCases,
+  ],
+})
+export class AuthModule {}
